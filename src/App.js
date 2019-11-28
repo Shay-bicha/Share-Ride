a4ec114dd24fc012fe067f699ee45648b60419c2
Aschaley Ascheneke <45295709+aschalegn@users.noreply.github.com>
2019-11-28 13:22:59 +0200

    nnn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const getRedes = require('./getRideModule');
+const getRides = require('./getRideModule');
 const offerRides = require('./offerRides');
 const port = 8080;
 const publicdir = path.join(__dirname, '..', 'public');
@@ -12,8 +12,8 @@ app.post('/offerRides', (req, res)=>{
     offerRides.offerRides(req, res)
 })
 
-app.get('/getRides', (req, res)=>{
-    getRedes.getRides(req, res)
+app.post('/getRides', (req, res)=>{
+    getRides.getRides(req, res)
 })
 
 app.listen(port,()=>{
