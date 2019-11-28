f20972aa4146ead8c6a482074357eb503ef3be53
Shay <55179425+Shay-bicha@users.noreply.github.com>
2019-11-28 17:05:18 +0200

    dd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const getRides = require('./getRideModule');
-const offerRides = require('./offerRides');
+// const getRides = require('./getRideModule');
+// const offerRides = require('./offerRides');
 // const port = 8080;
-const signIN = require('./signin');
+// const signIN = require('./signin');
 const port = 8080;
-const publicdir = path.join(__dirname, '..', 'public');
-app.use(express.static(publicdir));
-app.use(express.json());
+// const publicdir = path.join(__dirname, '..', 'public');
+// app.use(express.static(publicdir));
+// app.use(express.json());
 
-app.post('/signin', (req, res)=>{
-    signin.signinHandler(req, res);
-});
+// app.post('/signin', (req, res)=>{
+//     signin.signinHandler(req, res);
+// });
 
-app.post('/offerRides', (req, res)=>{
-    offerRides.offerRides(req, res);
+// app.post('/offerRides', (req, res)=>{
+//     offerRides.offerRides(req, res);
     
-});
+// });
 
-app.post('/getRides', (req, res)=>{
-    getRides.getRides(req, res);
+// app.post('/getRides', (req, res)=>{
+//     getRides.getRides(req, res);
+// });
+app.get('/' , (req , res)=>{
+    res.send('hello world');
 });
 
 app.listen(process.env.PORT || 8080,()=>{
