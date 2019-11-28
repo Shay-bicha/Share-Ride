c97dfd7deff5f22f119ada8596e453753a530494
Aschaley Ascheneke <45295709+aschalegn@users.noreply.github.com>
2019-11-28 16:49:44 +0200

    offer rideserver side work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const path = require('path');
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 const app = express();
 const getRides = require('./getRideModule');
 const offerRides = require('./offerRides');
-const signIN = require('./signin');
+const signiN = require('./signin');
 const port = 8080;
+
+//! DB
+const uri = "mongodb+srv://aschaley123:As123123@cluster0-c0kmd.mongodb.net/shareRide?retryWrites=true&w=majority";
+mongoose.connect(uri,{useUnifiedTopology: true, useNewUrlParser: true},(err, db)=>{
+    if(err) throw err
+    else{
+        console.log("connected to Db");
+    }
+});
+
 const publicdir = path.join(__dirname, '..', 'public');
 app.use(express.static(publicdir));
 app.use(express.json());
 
 app.post('/signin', (req, res)=>{
-    signin.signinHandler(req, res);
+    signiN.signinHandler(req, res);
 });
 
 app.post('/offerRides', (req, res)=>{
     offerRides.offerRides(req, res);
-    
 });
 
 app.post('/getRides', (req, res)=>{
