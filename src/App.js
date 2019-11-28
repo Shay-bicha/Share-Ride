5c58be2b3e6d2b9663cc1fc55d599b02d4df6aa1
Aschaley Ascheneke <45295709+aschalegn@users.noreply.github.com>
2019-11-28 13:03:20 +0200

    src and public folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const getRedes = require('./getRideModule');
+const offerRides = require('./offerRides');
 const port = 8080;
 const publicdir = path.join(__dirname, '..', 'public');
 app.use(express.static(publicdir));
+app.use(express.json());
 
+app.get('/offerRides', (req, res)=>{
+    offerRides.offerRides(req, res)
+})
 
+app.get('/getRides', (req, res)=>{
+    getRedes.getRedes(req, res)
+})
 
 
 app.listen(port,()=>{
---
+++ src/offerRides.js
@@ -0,0 +1,11 @@
+//* imports fs and file
+const fs = require('fs');
+const fileName = './Rides.txt';
+
+exports.offerRedes = (req, res) => {
+    //* get data from user
+    let body = req.body;
+    console.log(body);
+
+    
+}
