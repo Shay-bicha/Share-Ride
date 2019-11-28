008dd463b28d7211058da1723dca2dda36208abe
Aschaley Ascheneke <45295709+aschalegn@users.noreply.github.com>
2019-11-29 01:14:21 +0200

    status response

--- src/userRoute.js
+++ src/userRoute.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const User = require('./models/userModel');
 exports.signinHandler = (req, res) => {
     let body = req.body;
-    if (body.password=== body.confirmPassword) {
+    if (body.password === body.confirmPassword) {
         // console.log(body);
         let newUser = {
             firstName: body.firstName,
@@ -14,14 +14,15 @@ exports.signinHandler = (req, res) => {
             age: body.age
         }
         console.log(newUser);
-        
-        User.create(newUser, (err, success)=>{
-            if(err) console.log(err);
-            else{
+
+        User.create(newUser, (err, success) => {
+            if (err) console.log(err);
+            else {
                 res.status(201).json({
                     data: "signed in successfully"
                 })
             }
+
         });
     };
 }
@@ -29,22 +30,22 @@ exports.signinHandler = (req, res) => {
 exports.loginHandler = (req, res) => {
     let body = req.body;
     try {
-        let user = User.findOne({email: body.email})
+        let user = User.findOne({ email: body.email })
         user.select('email name password');
-        user.exec((err, txt)=>{
-            if(err) console.log(err);
-            else{
+        user.exec((err, txt) => {
+            if (err) console.log(err);
+            else {
                 res.status(200).json({
                     data: "loged suseesfully"
                 });
             }
         });
-        
+
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
 
-  
+
 
