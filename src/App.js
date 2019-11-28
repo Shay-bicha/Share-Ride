283a22ceaafd10591d7775b599e6895231131f4f
Shay <55179425+Shay-bicha@users.noreply.github.com>
2019-11-28 15:54:33 +0200

    x

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const getRides = require('./getRideModule');
 const offerRides = require('./offerRides');
-const port = 8080;
+// const port = 8080;
 const publicdir = path.join(__dirname, '..', 'public');
 app.use(express.static(publicdir));
 app.use(express.json());
@@ -16,6 +16,6 @@ app.post('/getRides', (req, res)=>{
     getRides.getRides(req, res);
 });
 
-app.listen(port,()=>{
+app.listen(process.env,()=>{
     console.log("listening on port" + port);
 });
