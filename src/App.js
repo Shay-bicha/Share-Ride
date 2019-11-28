21c2381bec94707ef1a8702567418565284cda08
Shay <55179425+Shay-bicha@users.noreply.github.com>
2019-11-28 17:51:53 +0200

    ff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const app = express();
 const test = require('./test');
-// const getRides = require('./getRideModule');
+const getRides = require('./getRideModule');
 const offerRides = require('./offerRides');
 // const port = 8080;
 // const signIN = require('./signin');
@@ -22,9 +22,9 @@ app.post('/offerRides', (req, res)=>{
     
 });
 
-// app.post('/getRides', (req, res)=>{
-//     getRides.getRides(req, res);
-// });
+app.post('/getRides', (req, res)=>{
+    getRides.getRides(req, res);
+});
 app.get('/' , (req , res)=>{
     res.send('hello world');
 });
