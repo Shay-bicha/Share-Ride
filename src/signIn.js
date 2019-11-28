d88d7d002230fb7f63893831d7cab7a194f490a3
Aschaley Ascheneke <45295709+aschalegn@users.noreply.github.com>
2019-11-28 21:28:19 +0200

    Rides db (#4)
    * test
    
    * mongo integration
    
    * mongo integration
    
    * check for module require
    
    * check for module require
    
    * check for module require
    
    * check for module require
    
    * check for module require
    
    * log in

--- src/signIn.js
+++ src/signIn.js
@@ -3,17 +3,19 @@ const User = require('./models/userModel');
 exports.signinHandler = (req, res) => {
     let body = req.body;
     if (body.password=== body.confirmPassword) {
-        // let newUser = {
-        //     firstName: body.firstName,
-        //     lastName: body.lastName,
-        //     email: body.email,
-        //     password: body.password,
-        //     confirmPassword: body.confirmPassword,
-        //     gender: body.gender,
-        //     age: body.age
-        // }
-
-        User.create(body, (err, success)=>{
+        // console.log(body);
+        let newUser = {
+            firstName: body.firstName,
+            lastName: body.lastName,
+            email: body.email,
+            password: body.password,
+            confirmPassword: body.confirmPassword,
+            gender: body.gender,
+            age: body.age
+        }
+        console.log(newUser);
+        
+        User.create(newUser, (err, success)=>{
             if(err) console.log(err);
             else{
                 console.log(success);
@@ -22,14 +24,16 @@ exports.signinHandler = (req, res) => {
     };
 }
 
-
 exports.loginHandler = (req, res) => {
     let body = req.body;
+   
     try {
-        let user = User.findOne({email: bosy.email})
+        let user = User.findOne({email: body.email})
         user.select('email name password');
-        user.exec();
-
+        user.exec((err, txt)=>{
+            if(err) console.log(err);
+            
+        });
         res.status(200).send("loged suseesfully");
     } catch (error) {
         
@@ -38,11 +42,4 @@ exports.loginHandler = (req, res) => {
 
 
   
-
-   
-
-
-
-
-
 
