5764c81011129482aaa808de5056d15051366223
Aschaley Ascheneke <45295709+aschalegn@users.noreply.github.com>
2019-11-29 03:42:02 +0200

    getRide response

--- src/offerRides.js
+++ src/offerRides.js
@@ -14,7 +14,7 @@ exports.offerRides = (req, res) => {
 
     let newRide = { startingPoint: startingPoint, destination: destination, seats: seats, time: time }
     console.log(newRide);
-    
+
     //! insert to the database
     rideScheme.create(newRide, (err, success) => {
         if (err) console.log(err);
@@ -28,18 +28,14 @@ exports.offerRides = (req, res) => {
 
 exports.getRides = (req, res) => {
     let body = req.body;
-    let ride = rideScheme.find({
-        startingPoint: body.startingPoint,
-        destination: body.destination,
-        time: body.time
-    });
+    let ride = rideScheme.find({ startingPoint: body.startingPoint });
 
     ride.select('startingPoint destination time');
     ride.exec((err, txt) => {
         if (err) console.log(err);
         else {
             console.log(txt);
-            
+
             res.status(201).JSON({
                 data: txt
             })
